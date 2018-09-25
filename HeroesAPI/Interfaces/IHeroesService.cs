407ccabef7ac28d3f3cdcb2bdbec5152a4c0a24d
Shane Swezey <shanedswezey@gmail.com>
2018-09-25 16:04:47 -0700

    Added new interface: IHeroesService

---
+++ HeroesAPI/Interfaces/IHeroesService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using HeroesAPI.Models;
+
+namespace HeroesAPI.Interfaces
+{
+    public interface IHeroesService
+    {
+        IEnumerable<Hero> GetAllHeroes();
+        Hero GetHeroById();
+        Hero GetHeroByName(string name);
+        Hero GetHeroByRole(string role);
+        Hero GetHeroByUniverse(string universe);
+    }
+}
